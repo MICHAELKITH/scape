85de2c325c43dc187efd4b5806520eb15b29e557
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-01-26 22:16:25 +0100

    12: favorite

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -70,7 +70,7 @@ export const BoardList = ({
             authorName={board.authorName}
             createdAt={board._creationTime}
             orgId={board.orgId}
-            isFavorite={false}
+            isFavorite={board.isFavorite}
           />
         ))}
       </div>
