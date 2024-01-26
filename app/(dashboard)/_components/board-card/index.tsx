5a207518fc9bbea006095d5aff28e3661f846f02
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-01-26 21:06:32 +0100

    11: card actions

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -3,8 +3,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 
+import { Actions } from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { Footer } from "./footer";
@@ -49,6 +51,19 @@ export const BoardCard = ({
             className="object-fit"
           />
           <Overlay />
+          <Actions
+            id={id}
+            title={title}
+            side="right"
+          >
+            <button
+              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+            >
+              <MoreHorizontal
+                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
