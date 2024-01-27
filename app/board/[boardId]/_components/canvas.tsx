aa0f14963420f59d21a28e2e3cb7e91f2e01ab11
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2024-01-27 15:49:01 +0100

    16: room auth

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,6 +4,8 @@ import { Info } from "./info";
 import { Toolbar } from "./toolbar";
 import { Participants } from "./participants";
 
+import { useSelf } from "@/liveblocks.config";
+
 interface CanvasProps {
   boardId: string;
 };
@@ -11,6 +13,10 @@ interface CanvasProps {
 export const Canvas = ({
   boardId,
 }: CanvasProps) => {
+  const info = useSelf((me) => me.info);
+
+  console.log(info)
+
   return (
     <main
       className="h-full w-full relative bg-neutral-100 touch-none"
